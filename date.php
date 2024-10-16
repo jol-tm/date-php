ed5722b8305819b095f3a8f4bec4f5354754f876
João Lucas Teixeira Magalhães <163040452+jol-tm@users.noreply.github.com>
2024-10-16 15:51:23 -0300

    Add files via upload

---
+++ date.php
@@ -0,0 +1,9 @@
+<style>
+    body {
+        text-align: center;
+    }
+</style>
+<?php
+    header("Refresh: 1");
+    echo "<h1>Data: " . date("Y/m/d H:i:s", time()) . "</h1>";
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+            height: 100vh;
+        }
+        iframe {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="date.php" frameborder="0"></iframe>
+</body>
+</html>
